471b93f81cd231a91fe101a441ec09616d1d6d90
Andrew Player <andrewplayer3@gmail.com>
2022-01-31 23:22:43 -0900

    cleaned and commented and what not

--- server.js
+++ server.js
@@ -2,20 +2,31 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 const session = require('express-session');
 const path = require('path');
-/*
+/*/
  * If you get a TextEncoder not Defined Error, you need to copy this
  * in the beginning of the file that gives the error:
  * const {TextEncoder, TextDecoder} = require('util');	
- */
+/*/
 const {MongoClient} = require('mongodb');
 
+/*/
+ *  Database Information - Change this if yours is different doesn't match.
+/*/
+const url = "mongodb://localhost:27017/";
+const client = new MongoClient(url);
+const dbName = "UserDB";
+const dbCollection = "users";
+client.connect();
+
+
+// We are using express as the web server.
 var server = express();
 
 
+/*/
+ *  The user's session (cookie).
+/*/
 server.use(session({
-   /*
-	*  The user's session (cookie).
-	*/
 	secret: '123456',
 	resave: true,
 	saveUninitialized: true
@@ -24,28 +35,25 @@ server.use(express.urlencoded({extended : true}));
 server.use(express.json());
 
 
+/*/
+ *  Load the initial login page that has the form.
+/*/
 server.get('/', function(request, response) {
-   /*
-	*  Load the initial login page that has the form.
-	*/
 	response.sendFile(path.join(__dirname + '/login.html'));
 });
 
 
+/*/
+ *  This accepts the post from the login for, and preforms the
+ *  password check against the result from the mongodb. If it 
+ *  is correct, the user gets redirected to /home, else they are
+ *  told the username or password is incorrect.
+/*/
 server.post('/authenticate', function(request, response) {
-   /*
-	*  This accepts the post from the login for, and preforms the
-	*  password check against the result from the mongodb. If it 
-	*  is correct, the user gets redirected to /home, else they are
-	*  told the username or password is incorrect.
-	*/
 	var username = request.body.username;
 	var password = request.body.password;
 	if (username && password) {
-		const url = "mongodb://localhost:27017/";
-		const client = new MongoClient(url);
-		client.connect();
-		client.db('UserDB').collection('users').find({'username' : username}).toArray(function(err, result) {
+		client.db(dbName).collection(dbCollection).find({'username' : username}).toArray(function(err, result) {
 			// User Format: {"username" : "user", "password" : "pwd"} 
 			if (err) throw err;
 			bcrypt.compare(password, result[0]['password'], function(err, result) {
@@ -67,11 +75,11 @@ server.post('/authenticate', function(request, response) {
 });
 
 
+/*/
+ *  This is the user's home page: they should only get
+ *  here if they are logged in.
+/*/
 server.get('/home', function(request, response) {
-   /*
-	*  This is the user's home page: they should only get
-	*  here if they are logged in.
-	*/
 	if (request.session.loggedin) {
 		response.send('You are ' + request.session.user + ' and you have logged in.');
 	} else {
@@ -79,5 +87,6 @@ server.get('/home', function(request, response) {
 	}
 	response.end();
 });
+
 
 server.listen(8080);
